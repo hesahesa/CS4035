24d1503ae6f70bd4ca66f7e76e5489f3fe60d6b8
hendrahc <hendra.h2c.x@gmail.com>
2016-05-30 14:57:09 +0200

    update output for cost in cluster_assignment

--- week4/code_skeleton/plot_kmeans/cluster_assignment.m
+++ week4/code_skeleton/plot_kmeans/cluster_assignment.m
@@ -2,7 +2,7 @@ function [Cost,idx_cluster] = cluster_assignment(data, centroids)
 Nrecords = size(data,1);
 K=size(centroids,1);
 
-Cost = [];
+Cost = 0;
 idx_cluster = [];
     
 for i=1:Nrecords
@@ -12,8 +12,10 @@ for i=1:Nrecords
     diff = ones(K,1)*current_data - centroids;
     dist = (sqrt(sum((diff').^2,1))).^2; %norm^2 for each cluster distance
     
+    % accumulate the cost
+    Cost = Cost + sum(dist);
+    
     % get the nearest centroid
     [Min IMin] = min(dist,[],2);
-    Cost(i,1) = Min;
     idx_cluster(i,1) = IMin;
 end
