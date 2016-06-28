2927d498191608b4fafa2a57891fc8552c86e65d
Prahesa K Setia <prahesa@yahoo.com>
2016-06-28 15:33:10 +0200

    update sqldf

--- week7/cda-week7-hendra-prahesa.R
+++ week7/cda-week7-hendra-prahesa.R
@@ -5,8 +5,8 @@ data <- read.table("data_for_student_case.csv", sep = ",", header=TRUE)
 data <- data[complete.cases(data),]
 data$creationdate <- weekdays(as.Date(data$creationdate))
 
-#sqldf('update data set issuercountrycode="A",  shoppercountrycode="B" where issuercountrycode<>shoppercountrycode')
-#sqldf('update data set issuercountrycode="A",  shoppercountrycode="A" where issuercountrycode=shoppercountrycode')
+data <- sqldf(c('update data set issuercountrycode="A",  shoppercountrycode="B" where issuercountrycode<>shoppercountrycode', 'select * from main.data'))
+data <- sqldf(c('update data set issuercountrycode="A",  shoppercountrycode="A" where issuercountrycode=shoppercountrycode', 'select * from main.data'))
 sqlres <- sqldf('select issuercountrycode,shoppercountrycode,shopperinteraction, creationdate, count(*) from data group by issuercountrycode,issuercountrycode,shoppercountrycode,shopperinteraction,creationdate order by count(*) ')
 
 sdc <- createSdcObj(data, keyVars = c("shoppercountrycode", "issuercountrycode", "shopperinteraction", "creationdate"), sensibleVar = c("card_id"))
